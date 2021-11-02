2c2318506612d405cd96ae6c9433c36ac7a95b1a
John Roberts <roberts.john@gmail.com>
2021-11-01 17:59:39 -0700

    users migration

--- migrations/20211102004933-users.js
+++ migrations/20211102004933-users.js
@@ -1,22 +1,23 @@
 "use strict";
 
+const TABLE_NAME = "users";
+
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
+  up: async (queryInterface, DataTypes) => {
     /**
      * Add altering commands here.
      *
      * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     * await queryInterface.createTable('users', { id: DataTypes.INTEGER });
      */
-    return queryInterface.createTable("users", {});
+    return queryInterface.createTable(TABLE_NAME, {
+      id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+      email: { type: DataTypes.STRING, allowNull: false, unique: true },
+      password: { type: DataTypes.STRING, allowNull: false },
+    });
   },
 
-  down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+  down: async (queryInterface, DataTypes) => {
+    return queryInterface.dropTable(TABLE_NAME);
   },
 };
