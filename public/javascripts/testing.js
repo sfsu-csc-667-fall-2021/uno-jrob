ce8dbc8de7635ba022ce4ddd8b1ae72adbbadedf
John Roberts <roberts.john@gmail.com>
2021-11-02 12:53:01 -0700

    adding ability to view table contents

---
+++ public/javascripts/testing.js
@@ -0,0 +1,19 @@
+document.querySelector("#table-list").addEventListener("click", (event) => {
+  event.preventDefault();
+
+  const { tableName } = event.target.dataset;
+
+  if (tableName === undefined) {
+    return;
+  }
+
+  Array.from(document.querySelector(`div#tableContainer`).children).forEach(
+    (element) => {
+      console.log(element.dataset.tableName, tableName);
+      if (element.dataset.tableName === tableName) {
+        element.classList.remove("hidden");
+      } else if (!element.classList.contains("hidden"))
+        [element.classList.add("hidden")];
+    }
+  );
+});
--- routes/tests.js
+++ routes/tests.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const db = require("../db");
+const tables = require("../db/table_names");
 
 router.get("/", (request, response) => {
   db.any(
@@ -8,6 +9,29 @@ router.get("/", (request, response) => {
   )
     .then((_) => db.any("SELECT * FROM test_table"))
     .then((result) => response.json(result))
+    .catch((error) => {
+      console.log(error);
+      response.json({ error });
+    });
+});
+
+router.get("/tables", (request, response) => {
+  Promise.all(
+    Object.keys(tables).map((tableName) =>
+      db.any(`SELECT * FROM ${tables[tableName]}`).then((result) => ({
+        tableName,
+        result,
+        fields: Object.keys(result[0] || {}),
+      }))
+    )
+  )
+    .then((results) => {
+      console.log("RESULTS", results);
+      response.render("test/tables", {
+        tables: Object.keys(tables).map((t) => tables[t]),
+        results,
+      });
+    })
     .catch((error) => {
       console.log(error);
       response.json({ error });
